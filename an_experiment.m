71d5c83e65473e321ca418822aaf88fac2dbbb4c
WaiteWiedeman <119641244+WaiteWiedeman@users.noreply.github.com>
2024-11-26 20:40:01 -0500

    Add files via upload

--- an_experiment.m
+++ an_experiment.m
@@ -4,21 +4,12 @@ clear;
 clc;
 sysParams = params_system();
 ctrlParams = params_control();
-global trainParams;
 trainParams = params_training();
-trainParams.numSamples = 500;
-trainParams.type = "lstm6";
-trainParams.numEpochs = 50;
-trainParams.initLearningRate = 1e-3;
-trainParams.stopLearningRate = 1e-5;
-trainParams.miniBatchSize = 2000;
-trainParams.lrDropEpoch = 1;
-
-%% plot system motion with a sample
-% f1Max = 15;
-% tSpan = [0,5];
-% len = plot_system(sysParams, ctrlParams, f1Max, tSpan);
-% disp(num2str(len)+" time steps");
+trainParams.numSamples = 400;
+trainParams.type = "dnn4"; % "dnn4","lstm4","pinn4","dnn6", "lstm6","pinn6"
+trainParams.numLayers = 4;
+trainParams.numNeurons = 256;
+modelFile = "model\"+trainParams.type+"_"+num2str(trainParams.numLayers)+"_"+num2str(trainParams.numNeurons)+".mat";
 
 %% generate samples
 if ~exist("\data\", 'dir')
@@ -35,70 +26,50 @@ if ~exist("\model\", 'dir')
    mkdir("model");
 end
 switch trainParams.type
-    case "dnn2"
-        [modelFile, trainLoss] = train_dnn_model_2(dataFile, trainParams);
-    case "lstm2"
-        [modelFile, trainLoss] = train_lstm_model_2(dataFile, trainParams);
-    case "pinn2"
-        [modelFile, trainLoss] = train_pinn_model_2(dataFile, trainParams);
     case "dnn4"
-        [modelFile, trainLoss] = train_dnn_model_4(dataFile, trainParams);
+        [xTrain,yTrain,layers,options] = train_dnn_model_4(dataFile, trainParams);
     case "lstm4"
-        [modelFile, trainLoss] = train_lstm_model_4(dataFile, trainParams);
+        [xTrain,yTrain,layers,options] = train_lstm_model_4(dataFile, trainParams);
     case "pinn4"
-        [modelFile, trainLoss] = train_pinn_model_4(dataFile, trainParams);
+        [xTrain,yTrain,layers,options] = train_pinn_model_4(dataFile, trainParams);
     case "dnn6"
-        [modelFile, trainLoss] = train_dnn_model_6(dataFile, trainParams);
+        [xTrain,yTrain,layers,options] = train_dnn_model_6(dataFile, trainParams);
     case "lstm6"
-        [modelFile, trainLoss] = train_lstm_model_6(dataFile, trainParams);
+        [xTrain,yTrain,layers,options] = train_lstm_model_6(dataFile, trainParams);
     case "pinn6"
-        [modelFile, trainLoss] = train_pinn_model_6(dataFile, trainParams);
-    % case "pirn2"
-    %     [modelFile, trainLoss] = train_pirn_model_2(dataFile, trainParams);
-    % case "pirn4"
-    %     [modelFile, trainLoss] = train_pirn_model_4(dataFile, trainParams);
-    % case "pirn6"
-    %     [modelFile, trainLoss] = train_pirn_model_6(dataFile, trainParams);
+        [xTrain,yTrain,layers,options] = train_pinn_model_6(dataFile, trainParams);
     otherwise
         disp("unspecified type of model.")
 end
 
-%% plot training curve
-% figure('Position',[500,100,800,400]); 
-% tiledlayout("vertical","TileSpacing","tight")
-% x = 1:length(trainLoss);
-% y = trainLoss(x);
-% smoothed_y = smoothdata(y,'gaussian');
-% plot(x,y,'b-',x,smoothed_y,'r-',"LineWidth",2);
-% xlabel("Iteration","FontName","Arial")
-% ylabel("Loss","FontName","Arial")
-% legend("Original","Smoothed","location","best")
-% set(gca, 'FontSize', 15);
+plot(layers)
+% training with numeric array data
+[net,info] = trainNetwork(xTrain,yTrain,layers,options);
+trainLoss = info.TrainingLoss;
+save(modelFile, 'net');
+% disp(info)
 
-%% model evaluation
-% disp("evaluating trained model...")
-f1Max = [10,40];
-tSpan = [0,10];
-predIntervel = 10;
+%% simulation 
+f1 = 20; % initial force input
+numTime = 100;
+tSpan = [0,5]; % [0,5] 0:0.01:5
+predInterval = tSpan(2); 
+ctrlParams.fMax = [f1; 0];
+y = sdpm_simulation(tSpan, sysParams, ctrlParams);
+t = y(:,1);
+x = y(:,2:7);
+[xp, rmseErr, refTime] = evaluate_single(net, t, x, ctrlParams, trainParams, tSpan, predInterval, numTime, trainParams.type);
+plot_compared_states(t,x,t,xp)
+sdpm_snapshot(sysParams, t, x(:,1), x(:,2), xp(:,1), xp(:,2), 3)
+sdpm_animation(sysParams, t, x(:,1), x(:,2), xp(:,1), xp(:,2))
+disp(mean(rmseErr,'all'))
+
+%% evaluate for four states
+f1Max = [5,30];
+tSpan = [0,5];
+predIntervel = 5;
 numCase = 50;
 numTime = 100;
-avgRMSE = evaluate_model_with_4_states(modelFile, sysParams, ctrlParams, trainParams, f1Max, tSpan, predIntervel, numCase, numTime, trainParams.type);
-disp(["average rmse", avgRMSE])
-
-%% plot single prediction
-disp("plot prediction..."+modelFile)
-f1Max = 25;
-tSpan = [0,10];
-predIntervel = 10;
-plot_prediction(modelFile, sysParams, ctrlParams, trainParams, f1Max, tSpan, predIntervel, trainParams.type);
-
-%% plot comparision
-% folder = "model";
-% typeList = ["dnn4","lstm4","pinn4","dnn6"];
-% trainParams.numSamples = 50;
-% numState = 4;
-% f1Max = 25;
-% tSpan = [0,10];
-% predInterval = 10;
-% numTime = 100;
-% res = compare_model(folder, typeList, sysParams, ctrlParams, trainParams, f1Max, tSpan, predInterval, numTime, numState);
+%avgErr = evaluate_model(net, sysParams, ctrlParams, trainParams, f1Max, tSpan, predInterval, numCase, numTime, trainParams.type);
+avgErr = evaluate_model_with_4_states(net, sysParams, ctrlParams, trainParams, f1Max, tSpan, predInterval, numCase, numTime, trainParams.type);
+disp(avgErr)
